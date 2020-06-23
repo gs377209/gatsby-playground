5e25a4a17120849c56c04bc9b9e63e970859c194
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2020-06-23 16:07:07 -0400

    adding graphql stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,11 @@
  */
 
 module.exports = {
+	siteMetadata: {
+		title: `My Sweet Site`,
+	  },
 	plugins: [
+	`gatsby-plugin-emotion`,
 	  {
 		resolve: `gatsby-plugin-typography`,
 		options: {
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,24 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Header from "../components/header"
 import Layout from "../components/layout"
 
-export default function About() {
+export default function About({ data }) {
   return (
     <Layout>
-      <Header headerText="About Gatsby" />
-      <Header headerText="It's pretty cool" />
+
+      <Header headerText={`About ${data.site.siteMetadata.title}`} />
       <p>Such wow. Very React.</p>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
