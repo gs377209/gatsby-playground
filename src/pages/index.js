a5f88078d05664c3d11b11ea357fb253ae5faf27
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2020-06-24 15:24:47 -0400

    generating pages dynamically

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 import { css } from "@emotion/core"
 import { rhythm } from "../utils/typography"
 import Header from "../components/header"
@@ -16,21 +16,29 @@ export default function Home({ data }) {
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-            <h3
+			<Link
+              to={node.fields.slug}
               css={css`
-                margin-bottom: ${rhythm(1 / 4)};
+                text-decoration: none;
+                color: inherit;
               `}
             >
-              {node.frontmatter.title}{" "}
-              <span
-                css={css`
-                  color: #bbb;
-                `}
-              >
-                — {node.frontmatter.date}
-              </span>
-            </h3>
-            <p>{node.excerpt}</p>
+				<h3
+				css={css`
+					margin-bottom: ${rhythm(1 / 4)};
+				`}
+				>
+				{node.frontmatter.title}{" "}
+				<span
+					css={css`
+					color: #bbb;
+					`}
+				>
+					— {node.frontmatter.date}
+				</span>
+				</h3>
+				<p>{node.excerpt}</p>
+			</Link>
           </div>
         ))}
       </div>
@@ -48,6 +56,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+		  }
+		  fields {
+            slug
           }
           excerpt
         }
