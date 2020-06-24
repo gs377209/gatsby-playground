70161dc8a2a06be44d2c73e65a1d4a2c668ce1e3
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2020-06-24 11:22:17 -0400

    adding source plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,13 @@ module.exports = {
 		title: `My Sweet Site`,
 	  },
 	plugins: [
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+			  name: `src`,
+			  path: `${__dirname}/src/`,
+			},
+		  },
 	`gatsby-plugin-emotion`,
 	  {
 		resolve: `gatsby-plugin-typography`,
